b3cd6a18e85c7d7b8b2837d1504355e18aa7a6e0
Luis Gargitter <luis.gargitter@gmx.net>
2023-03-02 21:52:12 +0100

    implemented matrix multiplication

--- sdl2/linag.c
+++ sdl2/linag.c
@@ -3,26 +3,66 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <math.h>
 
 typedef struct matrix_t {
     uint32_t n;
     uint32_t m;
-    float** mem_v;
+    float* mem;
 } matrix_t;
 
-void matmult(const float matrix[DIM][DIM], const float vector[DIM], float out[DIM]) {
-    float acc[3] = {0.0, 0.0, 0.0}; 
-        // performancewise not the best option
-        // but dont want to take memory responsibility;
-    for(int i = 0; i < DIM; i++) {
-        for(int j = 0; j < DIM; j++) {
-            acc[i] += matrix[i][j] * vector[j];
+matrix_t * matrixNew(uint32_t m, uint32_t n, const float **fill) {
+    matrix_t *mat = malloc(sizeof(matrix_t));
+    if(mat == NULL) return NULL;
+    
+    mat->n = n;
+    mat->m = m;
+    mat->mem = malloc(sizeof(float *) * m * n);
+    if(mat->mem == NULL) {
+        free(mat);
+        return NULL;
+    }
+
+    if(fill == NULL) return mat;
+    uint32_t ind = 0;
+    for(uint32_t i = 0; i < m; i++) {
+        for(uint32_t j = 0; j < n; j++) {
+            mat->mem[ind] = fill[i][j];
+            ind++;
         }
     }
-    for(int i = 0; i < DIM; i++) {
-        out[i] = acc[i];
+
+    return mat;
+}
+
+void matrixFree(matrix_t *m) {
+    free(m->mem);
+    free(m);
+}
+
+int matrixMult(const matrix_t *n1, const matrix_t *n2, matrix_t *res) {
+    if(n1->n != n2->m || n1->m != res->m || n2->n != res->n) return -1; // rows and columns need to match
+    for(int i = 0; i < n1->m; i++) {
+        for(int j = 0; j < n2->n; j++) {
+            res->mem[i * res->m + j] = 0.0;
+            for(int k = 0; k < n1->n; k++) {
+                res->mem[i * res->m + j] += n1->mem[i * n1->m + k] * n2->mem[k * n2->m + j];
+            }
+        }
+    }
+
+    return 0;
+}
+
+void matrixDebug(const matrix_t *m) {
+    printf("Debug: Matrix at %p (%ux%u):\n", m, m->m, m->n);
+    for(int i = 0; i < m->m; i++) {
+        for(int j = 0; j < m->n; j++) {
+            printf("%f", m->mem[i * m->m + j]);
+        }
+        printf("\n");
     }
 }
 
