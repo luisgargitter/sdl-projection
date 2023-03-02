db8d68845baa7718b18f5f8a8d9f02ce1ad87013
Luis Gargitter <luis.gargitter@gmx.net>
2023-03-02 21:05:53 +0100

    added linag files and first functions

--- sdl2/main.c
+++ sdl2/main.c
@@ -15,26 +15,6 @@
 
 #define TITLE "Primitives"
 
-// linear algebra--------------
-void matmult(const float matrix[DIM][DIM], const float vector[DIM], float out[DIM]) {
-    float acc[3] = {0.0, 0.0, 0.0}; 
-        // performancewise not the best option
-        // but dont want to take memory responsibility;
-    for(int i = 0; i < DIM; i++) {
-        for(int j = 0; j < DIM; j++) {
-            acc[i] += matrix[i][j] * vector[j];
-        }
-    }
-    for(int i = 0; i < DIM; i++) {
-        out[i] = acc[i];
-    }
-}
-
-#if DIM == 3 // genrotmap only implemented for 3 dimensions
-void genrotmat(const float angles[DIM], float matrix[DIM][DIM]) {
-
-}
-#endif
 
 
 
