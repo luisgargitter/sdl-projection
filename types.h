9cedae20b03b2d93246bba75b1dbc3898baf27d3
bitSheriff <bitSheriff@protonmail.com>
2023-03-06 13:30:30 +0100

    added shorter standard types

--- types.h
+++ types.h
@@ -5,6 +5,14 @@
 #include <SDL2/SDL.h>
 #include "Error.h"  // the errors should be available everywhere
 
+/* shorter name tyes */
+typedef uint8_t   u8;
+typedef uint16_t  u16;
+typedef uint32_t  u32;
+typedef int8_t    i8;
+typedef int16_t   i16;
+typedef int32_t   i32;
+
 /**
  * @brief A 3D vector
 */
